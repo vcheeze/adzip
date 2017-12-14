dba415734c047229c28d839b5f04ad04b06014e4
Peter Chen <manchesterutd7@gmail.com>
2017-12-15 00:01:59 +0400

    getting input from command line

--- main.cpp
+++ main.cpp
@@ -9,7 +9,22 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-  // cout << "Creating the shared segment!" << endl;
+  // Getting arguments from the command line
+  for (int i = 1; i < argc; i += 2) {
+      if (strcmp(argv[i], "-c") == 0) {        // file to be sorted
+          // store files/directories into archive file
+      } else if (strcmp(argv[i], "-a") == 0) { // number of lines in file to be sorted
+          // append files/directories into archive file
+      } else if (strcmp(argv[i], "-x") == 0) { // number of sorter nodes to be created
+          // extract all files from archive file
+      } else if (strcmp(argv[i], "-m") == 0) { // name of sort executable
+          // print out meta data for all files/directories inside archive file
+      } else if (strcmp(argv[i], "-p") == 0) { // attribute number
+          // display hierarchy of files/directories inside archive file
+      } else {                                // no valid input provided
+          // invalid input
+      }
+  }
 
   return 0;
 }
